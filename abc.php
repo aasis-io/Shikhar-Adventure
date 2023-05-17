4560f1dbcf5dec2bfc761ffe3f5d2f89a3f2aaad
aasis-io <heyashishthapa@gmail.com>
2023-05-17 21:27:12 +0545

    Packages Updates

--- abc.php
+++ abc.php
@@ -16,16 +16,16 @@ include('navbar.php');
                 <li class="breadcrumb-item active">Annapurna Base Camp Trek</li>
             </ol>
         </div>
-    </div>
-    <div class="container">
+
         <div class="img-box"><img src="images/abc.jpg" alt=""></div>
-    </div>
 
 
 
-    <section class="trip-fact-price">
-        <div class="container">
+
+        <section class="trip-fact-price">
+
             <div class="row">
+
                 <div class="col-lg-8 col-md-8 col-sm-12">
                     <div class="package-bound package-intro">
                         <aside class="package-facts">
@@ -40,7 +40,7 @@ include('navbar.php');
                                 </li>
                                 <li>
                                     <dl>
-                                        <dt><span class="fa-solid fa-mountain"></span>Max Altitude</dt>
+                                        <dt><span class="fa-solid fa-mountain"></span>Max Elevation</dt>
                                         <dd>4310 Meters</dd>
                                     </dl>
                                 </li>
@@ -76,7 +76,7 @@ include('navbar.php');
                 <div class="col-lg-4 col-md-4 col-sm-12">
                     <div class="price-box">
                         <div class="price-tag">
-                            <span class="pr-title">Price:</span>
+                            <span class="pr-title">Starting At:</span>
                             <sup>$</sup>
                             <strong>
                                 1,080
@@ -90,9 +90,83 @@ include('navbar.php');
                         </div>
                     </div>
                 </div>
+
+            </div>
+
+        </section>
+        <aside class="it-link">
+            <ul>
+                <!-- <ion-icon name="caret-down-sharp"></ion-icon> -->
+                <li class="active"><a href="#trip-overview">Trip Overview</a></li>
+                <li><a href="">Itinerary</a></li>
+                <li><a href="">Trip Map</a></li>
+                <li><a href="">Cost Detail</a></li>
+                <li><a href="">Dates</a></li>
+                <li><a href="">Gallery</a></li>
+            </ul>
+        </aside>
+        <div class="row">
+            <div class="col-lg-8">
+                <section id="#trip-overview" class="about-trip">
+                    <div class="trip-title">
+                        <h3 class="title">Trip Overview</h3>
+                    </div>
+                    <div class="text-box">
+                        <p>
+                            The <strong> Annapurna Base Camp Trek </strong>is a breathtaking journey that takes you into the heart of the Annapurna Himalayas,
+                            offering awe-inspiring landscapes, rich cultural experiences, and stunning mountain vistas. This trek is a popular choice
+                            among adventure enthusiasts, offering a perfect blend of natural beauty, diverse terrain, and authentic Nepalese hospitality.
+                        </p>
+                        <p>
+                            <b> Duration: </b><br> The Annapurna Base Camp Trek typically takes around 10 to 14 days to complete, depending on your
+                            preferred itinerary and pace. It's essential to allow ample time for acclimatization to ensure a safe and enjoyable experience.
+                        </p>
+                        <p>
+                            <b> Highlights:</b><br> Scenic Landscapes: The trek showcases diverse landscapes, including lush rhododendron forests,
+                            terraced fields, cascading waterfalls, and picturesque traditional villages. You'll be captivated by the ever-changing natural beauty at every step.
+                        </p>
+                        <p><b> 1. Annapurna Sanctuary: </b>The ultimate highlight of the trek is reaching the Annapurna Base Camp, nestled at an
+                            altitude of 4,130 meters (13,550 feet). Here, you'll be surrounded by towering snow-capped peaks, including Annapurna I
+                            (8,091 meters) and Machhapuchhre (6,993 meters), creating a stunning amphitheater of mountains.
+                        </p>
+                        <p><b>2. Cultural Immersion: </b>Along the trail, you'll encounter local Gurung and Magar communities, known for their
+                            warm hospitality and unique cultural traditions. You'll have the opportunity to visit charming villages, interact with
+                            locals, and learn about their way of life.
+                        </p>
+                        <p><b>3. Hot Springs: </b> After days of trekking, you can relax your tired muscles in the natural hot springs at Jhinu Danda.
+                            The soothing thermal waters provide a rejuvenating experience, surrounded by lush greenery.
+                        </p>
+                        <p><b>4. Panoramic Viewpoints: </b> The Annapurna Base Camp Trek offers several vantage points that provide panoramic views
+                            of the Annapurna and Dhaulagiri Mountain ranges. Poon Hill (3,210 meters) is a popular viewpoint, offering stunning sunrise
+                            and sunset vistas.
+                        </p>
+                        <p> <b> Difficulty Level: </b><br>
+                            The trek is considered moderately challenging, suitable for trekkers with a moderate level of fitness. While no technical
+                            climbing skills are required, it does involve steep ascents and descents, as well as navigating some rugged terrain.
+                        </p>
+                        <p> <b> Permits: </b><br>
+                            Before starting the trek, you'll need to obtain necessary permits, including the Annapurna Conservation Area Permit
+                            (ACAP) and the Trekkers' Information Management System (TIMS) card. These permits can be arranged through authorized
+                            agencies or at the respective checkpoints along the trail.
+                        </p>
+                        <p> <b> Best Time to Trek: </b><br>
+                            The ideal seasons for the Annapurna Base Camp Trek are spring (March to May) and autumn (September to November).
+                            These months offer stable weather conditions, clear skies, and favorable temperatures for trekking. </p>
+                        <p> <b> Note: </b><br>
+                            It's always recommended to trek with a licensed guide or join a reputable trekking company to ensure safety,
+                            navigation assistance, and a more enriching experience.
+                            <br>
+                            The Annapurna Base Camp Trek promises an unforgettable adventure, immersing you in the natural and cultural
+                            wonders of the Annapurna region. Prepare to be mesmerized by the majestic mountains, stunning landscapes,
+                            and the warm hospitality of the Nepalese people as you embark on this incredible journey.
+                        </p>
+
+
+                    </div>
+                </section>
             </div>
         </div>
-    </section>
+    </div>
 </main>
 
 
